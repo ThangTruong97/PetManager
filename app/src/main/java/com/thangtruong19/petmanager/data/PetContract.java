acb518c71a3ad765a865d088b5f6f6b097a70765
ThangTruong97 <36329889+ThangTruong97@users.noreply.github.com>
2018-07-18 22:18:27 +0700

    Set content provider and add Uri to contract class

--- app/src/main/java/com/thangtruong19/petmanager/data/PetContract.java
+++ app/src/main/java/com/thangtruong19/petmanager/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.thangtruong19.petmanager.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -9,7 +10,13 @@ import android.provider.BaseColumns;
 public final class PetContract {
     private PetContract(){}
 
+    private static final String CONTENT_AUTHORITY="com.thangtruong19.petmanager" ;
+    private static final Uri BASE_CONTENT_URI= Uri.parse("context://"+CONTENT_AUTHORITY);
+    private static final String PATH_PETS="pets";
+
     public static class PetEntry implements BaseColumns{
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI=Uri.withAppendedPath(BASE_CONTENT_URI,PATH_PETS);
         public static final String TABLE_NAME="pets";
         public static final String _ID=BaseColumns._ID;
         public static final String COLUMN_NAME="name";
